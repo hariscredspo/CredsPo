ad0133315281022fc429d075447b5f3dfc74ee8c
someone-existing <haris.sinfo16@gmail.com>
2021-10-05 17:56:25 +0200

    Add splash, login and profile switching using nav_graph

---
+++ app/src/main/java/com/haris/credspo/ui/login/LoginFragment.kt
@@ -0,0 +1,31 @@
+package com.haris.credspo.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.haris.credspo.R
+import com.haris.credspo.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_login_fragment_to_profile_fragment)
+        }
+    }
+
+}
