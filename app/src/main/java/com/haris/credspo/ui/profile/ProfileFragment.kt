d8b816042e7f5525e9c155e8986e853ebf39ba18
someone-existing <haris.sinfo16@gmail.com>
2021-10-17 23:43:43 +0200

    Change min sdk to 28, cache user data and fix logging out

--- app/src/main/java/com/haris/credspo/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/haris/credspo/ui/profile/ProfileFragment.kt
@@ -9,8 +9,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.gson.GsonBuilder
 import com.haris.credspo.R
 import com.haris.credspo.databinding.FragmentProfileBinding
+import com.haris.credspo.models.UserData
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
@@ -32,25 +34,38 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val sharedPrefs = requireContext().getSharedPreferences("prefs", Context.MODE_PRIVATE)
+        val userData = sharedPrefs.getString("USER_DATA", null)
         val token = sharedPrefs.getString("BEARER_TOKEN", null)
 
-        token?.let {
-            viewModel.getProfile(it)
-        }
+        userData?.let { json ->
+            val convertedJson = GsonBuilder().create().fromJson(json, UserData::class.java)
+            binding.profileLabelName.text = "${convertedJson.firstName} ${convertedJson.lastName}"
+        } ?: run {
+            token?.let { token ->
+                viewModel.getProfile(token)
 
-        viewModel.profileResponseData.observeForever { data ->
-            data?.let {
-                with(binding) {
-                    profileLabelName.text = "${it.firstName} ${it.lastName}"
-                    profileButtonLogout.setOnClickListener { logout() }
-                    profileButtonDelete.setOnClickListener { logout() }
-                    profileButtonActivityHistory.setOnClickListener { findNavController().navigate(R.id.action_profile_fragment_to_activity_history_fragment) }
+                viewModel.profileResponseData.observeForever { data ->
+                    data?.let {
+                        val convertedData = GsonBuilder().create().toJson(it)
+                        sharedPrefs.edit().putString("USER_DATA", convertedData).apply()
+                        binding.profileLabelName.text = "${it.firstName} ${it.lastName}"
+                    }
                 }
             }
+        }
+
+
+        with(binding) {
+            profileButtonLogout.setOnClickListener { logout() }
+            profileButtonDelete.setOnClickListener { logout() }
         }
     }
 
     private fun logout() {
         findNavController().navigate(R.id.action_profile_fragment_to_login_fragment)
+        requireContext().getSharedPreferences("prefs", Context.MODE_PRIVATE).edit()
+            .putString("BEARER_TOKEN", null)
+            .putString("USER_DATA", null)
+            .apply()
     }
 }
